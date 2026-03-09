249857bac81839d491258c62edfde2dc99873214
GioHinojosa <giovanni.hinojosa@campus.mccd.edu>
2026-03-09 09:22:03 -0700

    Car.java

--- Car.java
+++ Car.java
@@ -6,9 +6,9 @@ public class Car {
     private int year;
     private String fuelType;
     private String color;
-    private int mileage;
+    private double mileage;
 
-    public Car(int id, String brand, String model, int year, String fuelType, String color, int mileage) {
+    public Car(int id, String brand, String model, int year, String fuelType, String color, double mileage) {
         this.id = id;
         this.brand = brand;
         this.model = model;
@@ -18,35 +18,20 @@ public class Car {
         this.mileage = mileage;
     }
 
-    public int getId() {
-        return id;
-    }
-
     public String getBrand() {
         return brand;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public int getYear() {
-        return year;
     }
 
     public String getFuelType() {
         return fuelType;
     }
 
-    public String getColor() {
-        return color;
-    }
-
-    public int getMileage() {
+    public double getMileage() {
         return mileage;
     }
 
     public String toString() {
-        return id + " | " + brand + " | " + model + " | " + year + " | " + fuelType + " | " + color + " | " + mileage;
+        return id + " | " + brand + " | " + model + " | " + year + " | " +
+               fuelType + " | " + color + " | " + mileage;
     }
 }
